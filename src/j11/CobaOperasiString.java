91e02728e16554b7c17684df9df24120bf1f54c3
ARYA <ARYA@Arya>
2018-08-16 11:41:46 +0700

    Initial Commit

---
+++ src/j11/CobaOperasiString.java
@@ -0,0 +1,10 @@
+
+package j11;
+
+public class CobaOperasiString {
+    public static void main(String[] args)
+    {
+    String identitas = "Arya Yudistira / X RPL 3 / 08";
+    System.out.println("Identitas : "+ identitas);
+    }
+}
