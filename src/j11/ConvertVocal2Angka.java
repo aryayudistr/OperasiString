ac16e1de3135638ae155f09bf30edda8055f609d
ARYA <ARYA@Arya>
2018-08-16 15:15:45 +0700

    Initial Commit

---
+++ src/j11/ConvertVocal2Angka.java
@@ -0,0 +1,27 @@
+package j11;
+import java.util.Scanner;
+
+
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas){
+    
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (Vokal Ke Angka)\n");
+    }
+
+    public static void main(String[] args){
+    
+        String identitas = "Arya Yudistira / X RPL 3 / 08";
+
+        
+    }
+}
+        
+     
+
+
+        
+    
+    
+
