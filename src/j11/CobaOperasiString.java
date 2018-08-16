dd7d3e8a1987ca2d20226cf3280c9998910c3388
ARYA <ARYA@Arya>
2018-08-16 11:49:02 +0700

    Coba method length & isEmpty

--- src/j11/CobaOperasiString.java
+++ src/j11/CobaOperasiString.java
@@ -4,7 +4,15 @@ package j11;
 public class CobaOperasiString {
     public static void main(String[] args)
     {
-    String identitas = "Arya Yudistira / X RPL 3 / 08";
-    System.out.println("Identitas : "+ identitas);
+        String x = "Operasi";
+    System.out.println("isi variable x : "+ x);
+    System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+    
+    System.out.println("x adalah kosong : " + x.isEmpty());
+    
+    String y = "";
+    System.out.println("isi variable y : "+ y);
+    System.out.println("y adalah kosong : " + y.isEmpty());
+    
     }
 }
