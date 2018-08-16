39cc9abf499737d22c8dca2b7d12c433a50f3fd9
ARYA <ARYA@Arya>
2018-08-16 14:09:15 +0700

    Initial Commit

---
+++ src/j11/Praktikum1.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j11;
+
+/**
+ *
+ * @author ARYA
+ */
+public class Praktikum1 {
+    public static void main(String[] args)
+    {
+    String identitas = "Arya Yudistira / X RPL 3 / 08";
+    System.out.println("Identitas : "+ identitas);
+    
+        System.out.println(" ");
+    }
+}
